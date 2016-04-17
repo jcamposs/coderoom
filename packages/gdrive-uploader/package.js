2d74c4a5a9208d27d536bbdd747a53cca153de21
Jorge Campos <jorge.camposserrano@gmail.com>
2016-04-17 08:53:22 +0200

    Added gdrive packages for upload and download files

---
+++ packages/gdrive-uploader/package.js
@@ -0,0 +1,25 @@
+Package.describe({
+  name: 'jcamposs:gdrive-uploader',
+  version: '0.0.1',
+  // Brief, one-line summary of the package.
+  summary: 'Meteor package that installs the Google Drive driver for uploading files directly with XHR/CORS',
+  // URL to the Git repository containing the source code for this package.
+  git: '',
+  // By default, Meteor will default to using README.md for documentation.
+  // To avoid submitting documentation, set this field to null.
+  documentation: 'README.md'
+});
+
+Package.onUse(function(api) {
+  api.versionsFrom('1.1.0.2');
+  api.addFiles('gdrive-uploader.js', 'client');
+
+  if (api.export)
+    api.export('GDriveUploader');
+});
+
+Package.onTest(function(api) {
+  api.use('tinytest');
+  api.use('jcamposs:gdrive-uploader');
+  api.addFiles('gdrive-uploader-tests.js');
+});
