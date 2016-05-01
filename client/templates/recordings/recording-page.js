81716882b6395c97ffbfb20c455a17ec08e77c9a
Jorge Campos <jorge.camposserrano@gmail.com>
2016-05-01 17:28:59 +0200

    Sync recording medias and editor events with popcornJS

--- client/templates/recordings/recording-page.js
+++ client/templates/recordings/recording-page.js
@@ -1,28 +1,132 @@
-var videoLocal;
+var mainVideoElement,
+participantsContainerElement;
+var editor;
+var $pop;
+var recording;
 
 Template.recordingPage.rendered = function(){
-  videoLocal = document.getElementById('video-room__recorded');
+  recording = this.data;
+  console.log('Loading recording... ' + JSON.stringify(recording));
 
-  var r = this.data;
-  console.log('record' + JSON.stringify(r));
+  // Get DOM elements
+  mainVideoElement = document.getElementById('main-video');
+  mainVideoElement.onplaying = function() {
+    editor.getSession().getDocument().setValue("");
+    $pop.play();
+  };
+  participantsContainerElement = document.getElementsByClassName("video-room__participants")[0];
+  participantsContainerElement.style.visibility='hidden';
 
-  var videos = r.videos;
-  videos.forEach(function(video) {
-    if (video.user !== Meteor.user().services.google.email) {
-      download(video.file, function(src) {
-        var container = document.createElement('div');
-        container.className = 'video-room__participant video-room__participant--active';
-        var v = document.createElement('video');
-        v.src = src
-        v.autoplay = true;
-        container.appendChild(v);
-        $('.video-room__participants').append(container);
+  // Initialize editor
+  editor = ace.edit('editor');
+  editor.getSession().getDocument().setValue("");
+  console.log('Initialize editor...');
+
+  $pop = Popcorn("#main-video");
+
+  // Recording videos
+  var videos = recording.videos;
+  var mainVideo = videos.find(isMain);
+  var participantsVideos = videos.filter(isParticipant);
+
+  // Download main video
+  download(mainVideo.file, function(src) {
+    mainVideoElement.src = src;
+
+    syncEditor();
+
+    var participantsProcessed = 0;
+    // Download participants Videos
+    participantsVideos.forEach(function(video) {
+      download(video.file, function(srcVideo) {
+        addParticipant(srcVideo);
+        participantsProcessed++;
+
+        if(participantsProcessed === participantsVideos.length) {
+          syncMedia();
+          participantsContainerElement.style.visibility='visible';
+        }
       })
-    } else {
-      download(video.file, function(src) {
-        videoLocal.src = src;
-      });
-    }
+    });
+  });
+}
+
+function isMain(video) {
+  return video.user == Meteor.user().services.google.email;
+}
+
+function isParticipant(video) {
+  return video.user != Meteor.user().services.google.email;
+}
+
+function addParticipant(srcVideo) {
+  var container = document.createElement('div');
+  container.className = 'video-room__participant';
+  var videoElement = document.createElement('video');
+  videoElement.src = srcVideo;
+  videoElement.className = "participant";
+  container.appendChild(videoElement);
+  $('.video-room__participants').append(container);
+}
+
+function syncMedia() {
+  var streams = {
+    a: $pop,
+    b: Popcorn(".participant")
+  }
+
+  loadCount = 0,
+  events = "play pause timeupdate seeking".split(/\s+/g);
+  //sync(streams);
+  // iterate both media sources
+  Popcorn.forEach( streams, function( media, type ) {
+    // when each is ready...
+    media.on( "canplayall", function() {
+      // trigger a custom "sync" event
+      this.emit("sync");
+    // Listen for the custom sync event...
+    }).on( "sync", function() {
+      // Once both items are loaded, sync events
+      if ( ++loadCount == 2 ) {
+        streams.a.mute();
+        // Iterate all events and trigger them on the video B
+        // whenever they occur on the video A
+        events.forEach(function( event ) {
+          streams.a.on( event, function() {
+            // Avoid overkill events, trigger timeupdate manually
+            if ( event === "timeupdate" ) {
+              if ( !this.media.paused ) {
+                return;
+              }
+              streams.b.emit( "timeupdate" );
+              return;
+            }
+            if ( event === "seeking" ) {
+              streams.b.currentTime( this.currentTime() );
+            }
+            if ( event === "play" || event === "pause" ) {
+              streams.b[ event ]();
+           }
+          });
+        });
+      }
+    });
+  });
+}
+
+function syncEditor() {
+  //ejecutamos las funciones filtradas en el editor
+  _(recording.RC).each(function(e){
+    $pop.cue(e.timestamp, function() {
+      switch(e.action){
+        case 'insert':
+          editor.getSession().getDocument().insertMergedLines(e.range.start, e.val);
+          break;
+        case 'remove':
+          editor.getSession().getDocument().remove(e.range);
+          break;
+       }
+    });
   });
 }
 
@@ -38,5 +142,5 @@ function download(file, callback) {
 
   // Upload video
   downloader.download();
-  console.log('Downloading');
+  console.log('Downloading ' + file);
 }
