379c52869ec0eb647818dbfd8e6b9e2c419be701
Jorge Campos <jorge.camposserrano@gmail.com>
2016-11-02 20:11:54 +0100

    Removed select theme editor

--- client/templates/editor/editor-bar.js
+++ client/templates/editor/editor-bar.js
@@ -1,8 +1,4 @@
 Template.editorBar.helpers({
-  themes: function() {
-    return getThemes();
-  },
-
   modes: function() {
     return getModes();
   }
@@ -17,11 +13,6 @@ Template.editorBar.destroyed = function() {
 };
 
 Template.editorBar.rendered = function() {
-  var defaultTheme = getThemes().find(function(i) {
-    return i.name === 'Monokai';
-  });
-  setThemeEditor(defaultTheme);
-
   var defaultMode = getModes().find(function(i) {
     return i.name === 'Text';
   });
@@ -37,64 +28,8 @@ Template.editorBar.events({
     };
 
     setModeEditor(mode);
-  },
-
-  'click #editor__bar__select__theme .dropdown-menu li': function (e) {
-    var theme = {
-      name: $(e.target).text(),
-      module: $(e.target).data('theme')
-    };
-
-    setThemeEditor(theme);
   }
 });
-
-function getThemes() {
-  var themes = [
-    {name: 'Ambiance' ,module: 'ambiance'},
-    {name: 'Chaos' ,module: 'chaos'},
-    {name: 'Chrome' ,module: 'chrome'},
-    {name: 'Clouds' ,module: 'clouds'},
-    {name: 'Clouds Midnight' ,module: 'clouds_midnight'},
-    {name: 'Cobalt' ,module: 'cobalt'},
-    {name: 'Crimson Editor' ,module: 'crimson_editor'},
-    {name: 'Dawn' ,module: 'dawn'},
-    {name: 'Dreamweaver' ,module: 'dreamweaver'},
-    {name: 'Eclipse' ,module: 'eclipse'},
-    {name: 'GitHub' ,module: 'github'},
-    {name: 'Gruvbox' ,module: 'gruvbox'},
-    {name: 'idle Fingers' ,module: 'idle_fingers'},
-    {name: 'IPlastic' ,module: 'iplastic'},
-    {name: 'KatzenMilch' ,module: 'katzenmilch'},
-    {name: 'krTheme' ,module: 'kr_theme'},
-    {name: 'Kuroir' ,module: 'kuroir'},
-    {name: 'Merbivore' ,module: 'merbivore'},
-    {name: 'Mervibore Soft' ,module: 'mervibore_soft'},
-    {name: 'Mono Indrustrial' ,module: 'mono_industrial'},
-    {name: 'Monokai' ,module: 'monokai'},
-    {name: 'Pastel on dark' ,module: 'pastel_on_dark'},
-    {name: 'Solarized Dark' ,module: 'solarized_dark'},
-    {name: 'Solarized Light' ,module: 'solarized_light'},
-    {name: 'Terminal' ,module: 'terminal'},
-    {name: 'Textmate' ,module: 'textmate'},
-    {name: 'Tomorrow' ,module: 'tomorrow'},
-    {name: 'Tomorrow Night' ,module: 'tomorrow_night'},
-    {name: 'Tomorrow Night Blue' ,module: 'tomorrow_night_blue'},
-    {name: 'Tomorrow Night Bright' ,module: 'tomorrow_night_bright'},
-    {name: 'Tomorrow Night 80s' ,module: 'tomorrow_night_eighties'},
-    {name: 'Twilight' ,module: 'twilight'},
-    {name: 'Vibrant Ink' ,module: 'vibrant_ink'},
-    {name: 'XCode' ,module: 'xcode'}
-  ];
-  return themes;
-}
-
-function setThemeEditor(theme) {
-  $('#editor__bar__select__theme').find('.editor__bar__filtered__option').text(theme.name);
-
-  var editor = ace.edit('editor');
-  editor.setTheme('ace/theme/' + theme.module);
-}
 
 function getModes() {
   var modes = [
