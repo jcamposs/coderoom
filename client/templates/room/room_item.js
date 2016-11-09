eab58c6ce6615f895cb8e9c86ab1fd7ac2ff06bf
Jorge Campos <jorge.camposserrano@gmail.com>
2016-11-09 11:15:49 +0100

    Added option delete room created

--- client/templates/room/room_item.js
+++ client/templates/room/room_item.js
@@ -4,5 +4,17 @@ Template.roomItem.events({
 
     $('#inviteGuest.modal').attr('data-id', this._id);
     $('#inviteGuest.modal').modal('show');
+  },
+  'click .btn-js-delete-room': function(e) {
+    e.preventDefault();
+
+    var r = Rooms.findOne({_id: this._id});
+
+    $('#deleteRoom.modal').attr('data-id', this._id);
+
+    var content = 'Are you sure delete '+ r.name + '?';
+    $('#deleteRoom.modal .modal__text').html(content);
+
+    $('#deleteRoom.modal').modal('show');
   }
 });
