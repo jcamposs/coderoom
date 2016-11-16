396f1f4578f473d47afdce28fac81ae58453aaf7
Jorge Campos <jorge.camposserrano@gmail.com>
2016-11-16 13:03:41 +0100

    Removed insecure package

--- client/templates/notifications/notification_item.js
+++ client/templates/notifications/notification_item.js
@@ -28,6 +28,6 @@
 
 Template.notificationItem.events({
   'click .notification': function() {
-    Notifications.update(this._id, {$set: {read: true}});
+    Meteor.call('setNotificationState', this._id, true);
   }
 });
